05be2bed26cd07dd0ea81938becc35a9ae22f750
AssetKlips <asseto@klips.com>
2023-09-04 22:35:09 +0600

    dc-21 [add] blog migration

--- backend/src/DrimCity/WebApi/Database/AppDbContext.cs
+++ backend/src/DrimCity/WebApi/Database/AppDbContext.cs
@@ -6,6 +6,8 @@ namespace WebApi.Database;
 public class AppDbContext : DbContext
 {
     public DbSet<Post> Posts { get; set; }
+    
+    public DbSet<Blog> Blogs { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
@@ -14,6 +16,7 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         MapPost(modelBuilder);
+        MapBlog(modelBuilder);
     }
 
     private static void MapPost(ModelBuilder modelBuilder)
@@ -44,6 +47,41 @@ public class AppDbContext : DbContext
                 .HasMaxLength(Post.SlugMaxLength);
 
             post.HasIndex(x => x.Slug)
+                .IsUnique();
+        });
+    }
+    
+    private static void MapBlog(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Blog>(blog =>
+        {
+            blog.HasKey(x => x.Id);
+
+            blog.Property(x => x.Id)
+                .UseIdentityAlwaysColumn();
+
+            blog.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(Post.TitleMaxLength);
+
+            blog.Property(x => x.Content)
+                .IsRequired()
+                .HasMaxLength(Post.ContentMaxLength);
+
+            blog.Property(x => x.CreatedAt)
+                .IsRequired();
+            
+            blog.Property(x => x.UpdatedAt)
+                .IsRequired();
+
+            blog.Property(x => x.AuthorId)
+                .IsRequired();
+
+            blog.Property(x => x.Slug)
+                .IsRequired()
+                .HasMaxLength(Post.SlugMaxLength);
+
+            blog.HasIndex(x => x.Slug)
                 .IsUnique();
         });
     }
---
+++ backend/src/DrimCity/WebApi/Migrations/20230904162556_AddBlog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBlog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    Title = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    Content = table.Column<string>(type: "character varying(100000)", maxLength: 100000, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    AuthorId = table.Column<int>(type: "integer", nullable: false),
+                    Slug = table.Column<string>(type: "character varying(400)", maxLength: 400, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_Slug",
+                table: "Blogs",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
