4201f62ef059461d23d97d5d6000b341a3ebe78b
Masud NetDev <mukanmasud@gmail.com>
2023-09-21 21:45:23 +0600

    Refactoring configuration in entity<Post>(modelBuilder)

--- backend/src/DrimCity/DrimCity.WebApi/Database/EntitiesBuilder/Features/Posts/PostEntitiesBuilder.cs
+++ backend/src/DrimCity/DrimCity.WebApi/Database/EntitiesBuilder/Features/Posts/PostEntitiesBuilder.cs
@@ -12,12 +12,12 @@ public class PostEntitiesBuilder: IEntityTypeConfiguration<Post>
     {
         builder.ToTable("posts");
         builder.HasKey(post => post.Id);
-        builder.Property(post => post.Id).HasColumnName("Id").UseIdentityAlwaysColumn();
-        builder.Property(post => post.Title).IsRequired().HasMaxLength(Post.TitleMaxLength);
-        builder.Property(post => post.Content).IsRequired().HasMaxLength(Post.ContentMaxLength);
-        builder.Property(post => post.CreatedAt).IsRequired();
-        builder.Property(post => post.AuthorId).IsRequired();//TODO relations with Entity<Author> one<Author> to many<Post>
-        builder.Property(x => x.Slug).IsRequired().HasMaxLength(Post.SlugMaxLength);
+        builder.Property(post => post.Id).HasColumnName("id").UseIdentityAlwaysColumn();
+        builder.Property(post => post.Title).HasColumnName("title").IsRequired().HasMaxLength(Post.TitleMaxLength);
+        builder.Property(post => post.Content).HasColumnName("content").IsRequired().HasMaxLength(Post.ContentMaxLength);
+        builder.Property(post => post.CreatedAt).HasColumnName("createdAt").IsRequired();
+        builder.Property(post => post.AuthorId).HasColumnName("authorId").IsRequired();//TODO relations with Entity<Author> one<Author> to many<Post>
+        builder.Property(x => x.Slug).HasColumnName("slug").IsRequired().HasMaxLength(Post.SlugMaxLength);
         builder.HasIndex(post => post.Slug).IsUnique();
     }
 }
