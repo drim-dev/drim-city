33ffa6ff0a5408b392037498ad336b97734283b3
Sergey Rubtsov <rubtsov.sergey.v@gmail.com>
2023-10-28 19:29:44 +0200

    #65 - add swaggerUI only for development environment

--- backend/src/DrimCity/DrimCity/DrimCity.WebApi/Program.cs
+++ backend/src/DrimCity/DrimCity/DrimCity.WebApi/Program.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using Common.Web.Endpoints;
 using Common.Web.Errors;
 using Common.Web.Validation.Behaviors;
+using DrimCity.WebApi;
 using DrimCity.WebApi.Database;
 using DrimCity.WebApi.Features.Accounts.Extensions;
 using FluentValidation;
@@ -22,13 +23,11 @@ builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
 builder.AddAccounts();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options => { options.CustomSchemaIds(type => type.ToString().Replace("+", "_")); });
+builder.AddSwaggerForDevelopmentEnv();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerForDevelopmentEnv();
 
 app.MapProblemDetails();
 
