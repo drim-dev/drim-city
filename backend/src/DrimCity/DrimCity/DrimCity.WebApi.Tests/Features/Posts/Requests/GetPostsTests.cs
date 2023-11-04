7994c0e79a6d28fa3b7e8d1b7f3f157d36600cfc
Sergey Rubtsov <rubtsov.sergey.v@gmail.com>
2023-11-04 18:39:02 +0100

    #33 - get posts - Add pagination

--- backend/src/DrimCity/DrimCity/DrimCity.WebApi.Tests/Features/Posts/Requests/GetPostsTests.cs
+++ backend/src/DrimCity/DrimCity/DrimCity.WebApi.Tests/Features/Posts/Requests/GetPostsTests.cs
@@ -1,12 +1,15 @@
 using System.Net;
+using System.Web;
 using Common.Tests.Database.Harnesses;
 using Common.Tests.Http.Extensions;
 using Common.Tests.Http.Harnesses;
 using DrimCity.WebApi.Database;
 using DrimCity.WebApi.Domain;
+using DrimCity.WebApi.Features.Posts.Requests;
 using DrimCity.WebApi.Tests.Features.Posts.Contracts;
 using DrimCity.WebApi.Tests.Fixtures;
 using DrimCity.WebApi.Tests.Utils;
+using FluentAssertions.Equivalency;
 
 namespace DrimCity.WebApi.Tests.Features.Posts.Requests;
 
@@ -38,7 +41,7 @@ public class GetPostsTests : IAsyncLifetime
         httpResponse.StatusCode.Should().Be(HttpStatusCode.OK);
         responsePosts.Should().NotBeNullOrEmpty();
 
-        responsePosts.Should().BeEquivalentTo(posts, options => options.Excluding(post => post.Author));
+        responsePosts.Should().BeEquivalentTo(posts, PostEquivalencyConfig);
     }
 
     [Fact]
@@ -72,19 +75,50 @@ public class GetPostsTests : IAsyncLifetime
         responsePosts.Should().BeInDescendingOrder(x => x.CreatedAt);
     }
 
-    private async Task<(PostContract[]?, HttpResponseMessage httpResponse)> Act()
+    [Fact]
+    public async Task Should_return_posts_by_pages()
     {
-        var httpClient = _httpClient.CreateClient();
-        return await httpClient.GetTyped<PostContract[]>("/posts", CreateCancellationToken());
+        var post1 = await CreatePost(createdAt: 02.November(2023).AsUtc().AddHours(1));
+        var post2 = await CreatePost(createdAt: 02.November(2023).AsUtc().AddHours(2));
+
+        const int pageSize = 1;
+        const int pageNumber = 2;
+        var (responsePosts, httpResponse) = await Act(pageSize, pageNumber);
+
+        httpResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        responsePosts.Should().NotBeNullOrEmpty();
+
+        responsePosts.Should().NotContainEquivalentOf(post2, PostEquivalencyConfig);
+        responsePosts.Should().ContainEquivalentOf(post1, PostEquivalencyConfig);
     }
 
-    private async Task CreatePost(string? content = null, DateTime? createdAt = null)
+    private async Task<(PostContract[]?, HttpResponseMessage httpResponse)> Act(int? pageSize = null,
+        int? pageNumber = null)
+    {
+        var queryParameters = HttpUtility.ParseQueryString(string.Empty);
+        if (pageSize.HasValue)
+        {
+            queryParameters.Add("pageSize", pageSize.Value.ToString());
+        }
+
+        if (pageNumber.HasValue)
+        {
+            queryParameters.Add("pageNumber", pageNumber.Value.ToString());
+        }
+
+        var httpClient = _httpClient.CreateClient();
+        return await httpClient.GetTyped<PostContract[]>($"/posts?{queryParameters}", CreateCancellationToken());
+    }
+
+    private async Task<Post> CreatePost(string? content = null, DateTime? createdAt = null)
     {
         var account = CreateAccount();
         await _database.Save(account);
 
         var post = FakerFactory.CreatePost(account.Id, content, createdAt);
         await _database.Save(post);
+
+        return post;
     }
 
     private async Task<Post[]> CreatePosts(int count)
@@ -98,5 +132,55 @@ public class GetPostsTests : IAsyncLifetime
         await _database.Save(posts.Cast<object>().ToArray());
 
         return posts;
+    }
+
+    private static EquivalencyAssertionOptions<Post> PostEquivalencyConfig(EquivalencyAssertionOptions<Post> options)
+    {
+        return options.Excluding(post => post.Author);
+    }
+}
+
+public class GetPostsRequestValidatorTests
+{
+    private const int AnyValidPageSize = 10;
+    private const int AnyValidPageNumber = 1;
+
+    private readonly GetPosts.RequestValidator _requestValidator = new();
+
+    [Fact]
+    public void Should_not_have_errors_when_request_is_valid()
+    {
+        var request = new GetPosts.Request(AnyValidPageSize, AnyValidPageNumber);
+
+        var result = _requestValidator.TestValidate(request);
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Should_have_error_when_page_size_less_than_1(int pageSize)
+    {
+        //TODO: question: should we use AutoBogus faker for testing request validators?
+        var request = new GetPosts.Request(pageSize, AnyValidPageNumber);
+
+        var result = _requestValidator.TestValidate(request);
+
+        result.ShouldHaveValidationErrorFor(x => x.PageSize)
+            .WithErrorCode("posts:validation:page_size_must_be_greater_or_equal_one");
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Should_have_error_when_page_number_less_than_1(int pageNumber)
+    {
+        var request = new GetPosts.Request(AnyValidPageSize, pageNumber);
+
+        var result = _requestValidator.TestValidate(request);
+
+        result.ShouldHaveValidationErrorFor(x => x.PageNumber)
+            .WithErrorCode("posts:validation:page_number_must_be_greater_or_equal_one");
     }
 }
--- backend/src/DrimCity/DrimCity/DrimCity.WebApi/Features/Posts/Requests/GetPosts.cs
+++ backend/src/DrimCity/DrimCity/DrimCity.WebApi/Features/Posts/Requests/GetPosts.cs
@@ -1,7 +1,9 @@
 using Common.Web.Endpoints;
 using DrimCity.WebApi.Database;
+using DrimCity.WebApi.Features.Posts.Errors;
 using DrimCity.WebApi.Features.Posts.Extensions;
 using DrimCity.WebApi.Features.Posts.Models;
+using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
@@ -15,16 +17,30 @@ public static class GetPosts
         public void MapEndpoint(WebApplication app)
         {
             app.MapGet("/posts", async Task<Ok<IReadOnlyCollection<PostModel>>>
-                (IMediator mediator, CancellationToken cancellationToken) =>
+                (IMediator mediator, [AsParameters] QueryRequest queryRequest, CancellationToken cancellationToken) =>
             {
-                //todo: add pagination
-                var posts = await mediator.Send(new Request(), cancellationToken);
+                var request = new Request(queryRequest.PageSize ?? 10, queryRequest.PageNumber ?? 1);
+                var posts = await mediator.Send(request, cancellationToken);
                 return TypedResults.Ok(posts);
             });
         }
     }
 
-    public record Request : IRequest<IReadOnlyCollection<PostModel>>;
+    public record QueryRequest(int? PageSize, int? PageNumber);
+
+    public record Request(int PageSize, int PageNumber) : IRequest<IReadOnlyCollection<PostModel>>;
+
+    public class RequestValidator : AbstractValidator<Request>
+    {
+        public RequestValidator()
+        {
+            RuleFor(request => request.PageSize)
+                .GreaterThanOrEqualTo(1).WithErrorCode(PostsValidationErrors.PageSizeMustBeGreaterOrEqualOne);
+
+            RuleFor(request => request.PageNumber)
+                .GreaterThanOrEqualTo(1).WithErrorCode(PostsValidationErrors.PageNumberMustBeGreaterOrEqualOne);
+        }
+    }
 
     public class RequestHandler : IRequestHandler<Request, IReadOnlyCollection<PostModel>>
     {
@@ -37,8 +53,12 @@ public static class GetPosts
 
         public async Task<IReadOnlyCollection<PostModel>> Handle(Request request, CancellationToken cancellationToken)
         {
+            var pageIndex = request.PageNumber - 1;
+
             return await _dbContext.Posts
                 .OrderByDescending(x => x.CreatedAt)
+                .Skip(request.PageSize * pageIndex)
+                .Take(request.PageSize)
                 .Select(x => new PostModel(x.Id, x.Title, x.Content.Ellipsize(2000), x.CreatedAt, x.AuthorId, x.Slug))
                 .ToListAsync(cancellationToken);
         }
